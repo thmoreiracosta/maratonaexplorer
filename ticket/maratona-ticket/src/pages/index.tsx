9eb2e137ff9af94cf9b4e5737a5f2956846fc24f
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-06-15 11:03:22 -0300

    hoje

--- ticket/maratona-ticket/src/pages/index.tsx
+++ ticket/maratona-ticket/src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+import * as Dialog from "@radix-ui/react-dialog";
+
+import { Ticket } from "../components/Ticket";
+
 export default function Home() {
-  return <h1 className="text-purple-500">Hello World!</h1>;
+  const hasAvatarUploaded = true;
+
+  return (
+    <Dialog.Root>
+      <main className="lg:h-screen max-w-[480px] lg:max-w-[1240px] p-12 mx-auto flex items-center"></main>
+      <div></div>
+    </Dialog.Root>
+  );
 }
